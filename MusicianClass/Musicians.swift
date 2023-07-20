c1db4615d4fde82cc5df0f5b5ec77813d7e9024b
Süleyman Demir <suleymandemir75@hotmail.com>
2023-07-20 21:39:17 +0300

    Initial Commit

---
+++ MusicianClass/Musicians.swift
@@ -0,0 +1,15 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by eyüp yaşar demir on 15.05.2023.
+//
+
+import Foundation
+
+class Musicians {
+    var name = ""
+    var age = 0
+    var instument = ""
+     
+}
---
+++ MusicianClass/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by eyüp yaşar demir on 15.05.2023.
+//
+
+import Foundation
+
+print("Hello, World!")
+
